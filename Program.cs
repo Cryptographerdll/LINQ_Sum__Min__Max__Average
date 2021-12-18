691e13ef54ddeb1ade616a53c616bd72d87908cd
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-18 14:34:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LINQ_Sum__Min__Max__Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[] { 1, 45, 2, 3, 6, 12, 84 };
+
+            var count_Operation = Numbers.Sum();    // 153
+            // Numbers.Max();
+            // Numbers.Min();
+            // Numbers.Average();
+
+        }
+    }
+}
